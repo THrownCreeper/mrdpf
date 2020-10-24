2e31cb45808fe636fbba8b363780397eee3000c2
Jonathan Holtmann <jonathan@holtmann.xyz>
2020-10-24 16:55:54 -0700

    Create whitepaper template

---
+++ _whitepaper/make.bat
@@ -0,0 +1,15 @@
+del mrdpf_whitepaper.aux
+del mrdpf_whitepaper.bbl
+del mrdpf_whitepaper.blg
+del mrdpf_whitepaper.log
+del mrdpf_whitepaper.synctex*
+del *.pdf
+pdflatex mrdpf_whitepaper.tex
+bibtex mrdpf_whitepaper.aux
+pdflatex mrdpf_whitepaper.tex
+del mrdpf_whitepaper.aux
+del mrdpf_whitepaper.bbl
+del mrdpf_whitepaper.blg
+del mrdpf_whitepaper.log
+del mrdpf_whitepaper.synctex*
+mrdpf_whitepaper.pdf
