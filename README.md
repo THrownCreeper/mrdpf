11175e5db99868080175a449dfdea88ec3fc4657
Jonathan Holtmann <jonathan@holtmann.xyz>
2020-11-08 16:12:40 -0800

    Remove files from main

--- README.md
+++
@@ -1,2 +0,0 @@
-# mrdpf
-Mac RDP Forensics - Python module for parsing artifacts of the Microsoft RDP client for macOS
--- mrdpf/helpers.py
+++
@@ -1,8 +0,0 @@
-import dataclasses
-import json
-
-class DataclassJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if dataclasses.is_dataclass(obj):
-            return dataclasses.asdict(obj)
-        return super().default(obj)
